d73dd4141abf06ade535a8c153dbe50444227c9f
JustUsingaWebsite <112662750+JustUsingaWebsite@users.noreply.github.com>
2023-04-27 21:58:29 -0600

    Create main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "Pset_body.cpp"
+
+using namespace std;
+
+int main()
+{
+    int array[MySort::SIZE] = {100, 100, 100, 519, 37, 9999, 3, 1, 238};
+    /*
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+        [][][][][][][][][][]
+
+        [][][]
+    */
+
+    std::cout<<"Original array elements: \n";
+
+    for(int i = 0; i < MySort::SIZE; i++)
+        std::cout<<std::setw( 5 ) << array[ i ];
+
+    std::cout<<"\n\n";
+
+//return 0; //break
+
+    MySort sortObj;
+    sortObj.sort(array);
+
+    std::cout<<"\nArray elements in sorted order: \n";
+
+    for(int i = 0; i < MySort::SIZE; i++)
+        std::cout<<std::setw( 5 ) << array[ i ];
+
+    std::cout<<"\n";
+    //std::cin.get();
+
+    return 0;
+}
